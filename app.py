8c5836694442392d33657a0ac379b4e6c4d48229
Gabreil Freire <gabriellsf@gmail.com>
2019-07-06 10:14:18 -0300

    Push pre Banho

--- app.py
+++ app.py
@@ -46,11 +46,75 @@ bot = ChatBot('CPFL ChatBot',
     ]
 )
 
-conversa = ['Oi', 'Olá', 'Tudo bem?', 'Tudo ótimo', 
-        'Você gosta de programar?', 'Sim, eu programo em Python']
+conversas = []
+
+conversas.append(['Oi',
+    'Posso te ajudar com alguma coisa ?',
+    'Eu estou de mudança e preciso trocar o titular',
+    'Claro. Você sabe como fazer isso ?',
+    'Não',
+    'Alguem mora no endereço atualmente ? Preciso saber se lá já possui luz',
+    'Sim, moram lá.',
+    'Entendi, então faremos uma transferencia de titular. Para qual endereço você vai se mudar ?',
+    '$$endereço$$',
+    'Obrigado, agora preciso só que me confirme sua identidade. Pode me mandar uma foto sua segurando seu RG (com a foto pra camera)?',
+    '$$foto_usuario$$',
+    'Ficou ótimo, conseguimos te ver bem. Última coisa, você poderia mandar uma foto do contrato da casa, de aluguel ou algo do tipo ?',
+    '$$foto_contrato$$',
+    'Perfeito, já tenho todas as informações que preciso! Em breve ligaremos confirmando a sua troca de titularidade.'
+])
+
+conversas.append(['Olá',
+    'Posso te ajudar com alguma coisa ?',
+    'Quero realizar a troca de titular',
+    'Claro. Você sabe como fazer isso ?',
+    'Sim',
+    'Entendi, então faremos uma transferencia de titular. Para qual endereço você vai se mudar ?',
+    '$$endereço$$',
+    'Obrigado, agora preciso só que me confirme sua identidade. Pode me mandar uma foto sua segurando seu RG (com a foto pra camera)?',
+    '$$foto_usuario$$',
+    'Ficou ótimo, conseguimos te ver bem. Última coisa, você poderia mandar uma foto do contrato da casa, de aluguel ou algo do tipo ?',
+    '$$foto_contrato$$',
+    'Perfeito, já tenho todas as informações que preciso! Em breve ligaremos confirmando a sua troca de titularidade.'
+])
+
+conversas.append(['Olá',
+    'Posso te ajudar com alguma coisa ?',
+    'Quero fazer uma transferencia de titular',
+    'Claro. Você sabe como fazer isso ?',
+    'Sim',
+    'Entendi, então faremos uma transferencia de titular. Para qual endereço você vai se mudar ?',
+    '$$endereço$$',
+    'Obrigado, agora preciso só que me confirme sua identidade. Pode me mandar uma foto sua segurando seu RG (com a foto pra camera)?',
+    '$$foto_usuario$$',
+    'Ficou ótimo, conseguimos te ver bem. Última coisa, você poderia mandar uma foto do contrato da casa, de aluguel ou algo do tipo ?',
+    '$$foto_contrato$$',
+    'Perfeito, já tenho todas as informações que preciso! Em breve ligaremos confirmando a sua troca de titularidade.'
+])
+
+conversas.append(['Bom dia',
+    'Bom dia, posso te ajudar com alguma coisa ?',
+    'Vou fazer uma mudança.',
+    'Claro. Você sabe como fazer isso ?',
+    'Ainda não',
+    'Alguem mora no endereço atualmente ? Preciso saber se lá já possui luz',
+    'Não a casa é nova',
+    'Entendi, então faremos a instalação de um novo ponto. Para qual endereço você vai se mudar ?',
+    '$$endereço$$',
+    'Obrigado, agora preciso só que me confirme sua identidade. Pode me mandar uma foto sua segurando seu RG (com a foto pra camera)?',
+    '$$foto_usuario$$',
+    'Ficou ótimo, conseguimos te ver bem. você poderia mandar uma foto do contrato da casa, de aluguel ou algo do tipo ?',
+    '$$foto_contrato$$',
+    'Perfeito, só precisamos de mais uma informação. O pote próximo precisa estar de acordo com nosso regulamento como descrito nesse link, ele está de acordo?',
+    'Sim está',
+    'Você poderia nos mandar uma foto dele ?',
+    '$$foto_poste$$',
+    'Já tenho todas as informações que preciso! Em breve ligaremos confirmando a sua instalação.'
+])
 
 trainer = ListTrainer(bot)
-trainer.train(conversa)
+for conversa in conversas:    
+    trainer.train(conversa)
 
 #Rota Inicial
 @app.route('/')
@@ -114,18 +178,6 @@ def chat():
             data = r.json()
 
             confidence = data['confidence']
-
-
-
-
-            
-
-
-
-
-
-
-
 
             resp = {"text":"sucesso"}
             
